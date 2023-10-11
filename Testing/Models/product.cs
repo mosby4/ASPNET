0d59b9fc02b65d2da7b14b4514fad600368a2098
Mosby4 <rufus.hamby@gmial.com>
2023-10-11 11:32:15 -0500

    MVC 1 Completed

---
+++ Testing/Models/product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Testing.Models
+{
+	public class product
+	{
+		public product()
+		{
+		}
+		public int ProductID { get; set; }
+		public string Name { get; set; }
+		public double Price { get; set; }
+		public int CategoryId { get; set; }
+		public int OnSale { get; set; }
+		public int StockLevel { get; set; }
+
+	}
+}
+
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using Testing.Models;
+
+namespace Testing
+{
+	public class ProductRepository : IProductRepository
+	{
+		private readonly IDbConnection _conn;
+		public ProductRepository(IDbConnection conn)
+		{
+			_conn = conn;
+		}
+
+        public IEnumerable<product> GetAllProducts()
+        {
+			return _conn.Query<product>("Select * From Products;");
+        }
+    }
+}
